2afdf9adb2c349b1f8c05aa3f0ec71fb85fb34b0
Wassim Tajeddin <wassim.tajeddin@gmail.com>
2023-09-17 14:41:25 +0200

    Add files via upload
    Edit database

--- web_labb1/js/main.js
+++ web_labb1/js/main.js
@@ -3,15 +3,14 @@ const dataContainer = document.getElementById('dataContainer');
 fetch('database/books.json')
   .then((res) => res.json())
   .then((data) => {
-    // Display the first 8 books (or less if there are fewer than 8)
-    const booksToDisplay = data.slice(0, 8);
+ 
+    const booksToDisplay = data
 
-    // Iterate over the books to display data in the HTML structure
+
     booksToDisplay.forEach((book, index) => {
       const cardDiv = document.createElement('div');
       cardDiv.classList.add('card');
 
-      // Populate the card with book data
       cardDiv.innerHTML = `
         <img src="${book.imageUrl !== undefined ? book.imageUrl : "/images/book-undefined.jpg" }" alt="Book Image" class="card-image">
         <div class="card-title">
